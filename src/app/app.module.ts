5ba50670ec92c3b3820de305eeb139b94233f6bb
Curtis <20732674+CurtisDS@users.noreply.github.com>
2023-02-05 21:17:40 -0500

    pwa should be working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,9 +27,7 @@ export const DEBUG = false;
     DocsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     }) ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
